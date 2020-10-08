2632533cf34e9ff63a4fd35dd2faed41d7e13c29
Tuyet Le <toowee.le@gmail.com>
2020-10-08 10:07:16 +0100

    initialise state to update the value in preview

--- src/App.js
+++ src/App.js
@@ -2,10 +2,38 @@ import React, { Component } from "react";
 import "./App.scss";
 
 class App extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: "",
+    };
+  }
+
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <h1>Markdown Previewer</h1>
+
+        <div style={containerStyle} className="container">
+          <div className="col">
+            <h2>Editor</h2>
+            <div className="mark-input">
+              <textarea name="" id="editor" rows="10"></textarea>
+            </div>
+          </div>
+          <div className="col">
+            <h2>Previewer</h2>
+            <div id="preview"></div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
+
+const containerStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(2, 1fr)",
+};
 
 export default App;
