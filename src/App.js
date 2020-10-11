814143582d4ba0d0ff9eb991673e7baf8470497b
Tuyet Le <toowee.le@gmail.com>
2020-10-11 19:06:22 +0100

    update styling of app

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Markdown Previewer</h1>
+        <h1 className="title">Markdown Previewer</h1>
 
-        <div className="markdown-container">
+        <div className="app-container">
           <header>
             App.js - markdown-previewer
             <div className="buttons">
@@ -33,7 +33,7 @@ class App extends Component {
               <div className="button maximise"></div>
             </div>
           </header>
-          <div style={containerStyle} className="container">
+          <div style={containerStyle}>
             <div className="col markdown">
               <Editor
                 value={this.state.value}
@@ -41,41 +41,39 @@ class App extends Component {
               />
             </div>
             <div className="col preview">
-              <h2>Preview</h2>
+              <h2 className="subheading">Preview</h2>
               <div
+              style={previewStyle}
                 id="preview"
                 dangerouslySetInnerHTML={{ __html: marked(this.state.value) }}
               ></div>
             </div>
           </div>
-          <footer>Designed by Toowee</footer>
+          <footer>Designed by <a href="https://github.com/toowee-le/markdown-previewer" target="_blank" rel="noopener noreferrer" alt="Link to Github">Toowee</a></footer>
         </div>
       </div>
     );
   }
 }
 
-const placeholder = `
-  # Hello Dev!
-  ---
-  ## Welcome to my markdown previewer. Give it a try by editing the editor.
-
-  > Markdown is a lightweight markup language that you can use to add formatting elements to plaintext documents. It was created in 2004 by John Gruber and is now one of the world's most popular markup languages.
+const placeholder = `# Hello Dev!\n---\n\n## Welcome to my markdown previewer. Give it a try by editing the editor.\n\n> Markdown is a lightweight markup language that you can use to add formatting elements to plaintext documents. It was created in 2004 by John Gruber and is now one of the world's most popular markup languages.\n\nHere's what to do:
 
   * Type some markdown on the left
   * See HTML in the right
-  * **Cool right?**
-  
-  ![gif](https://media.giphy.com/media/MFYewrNPPkUzUUKVXa/giphy.gif)
-
-  You can also include a code block by wrapping the code snippet between three backticks on both sides.
-
-  This markdown previewer was built with React and uses the \`Marked.js\` library. Find the \`Marked.js\` documentation [here](https://marked.js.org/).
+  * **Cool right?**\n\n![gif](https://media.giphy.com/media/MFYewrNPPkUzUUKVXa/giphy.gif)\n\nYou can also include a code block by wrapping the code snippet between three backticks on both sides like so:\n\n\`\`\`console.log("Hello World, this is a code block.")\`\`\`\n\nThis markdown previewer was built with React and uses the \`Marked.js\` library. Find the \`Marked.js\` documentation [here](https://marked.js.org/).
 `;
 
 const containerStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(2, 1fr)",
+  maxWidth: "100%",
+  width: "100%"
 };
+
+const previewStyle = {
+  height: "100%",
+  textAlign: "left",
+  padding: ".2em 1em",
+}
 
 export default App;
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,17 +3,29 @@ import React from "react";
 export default function Editor(props) {
   return (
     <React.Fragment>
-      <h2>Markdown</h2>
+      <h2 className="subheading">Markdown Editor</h2>
       <div className="markdown-input">
         <textarea
+        style={textareaStyle}
           name="markdown"
           id="editor"
           className="editor"
           value={props.value}
           onChange={props.updateMarkdown}
-          rows={10}
+          rows={50}
         />
       </div>
     </React.Fragment>
   );
+}
+
+const textareaStyle = {
+    width: "95%",
+    marginTop: "1em",
+    border: "none",
+    background: "transparent",
+    color: "#20a0ee",
+    fontSize: "1.1rem",
+    textAlign: "left",
+    whiteSpace: "normal"
 }
