cea8104fcb8da96d8465e512518751082bf127f4
Tuyet Le <toowee.le@gmail.com>
2020-10-11 19:16:17 +0100

    Updates

---
+++ precache-manifest.712806a1ab797e6e5a28cc8e5a117969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65d25cb150a06fa528fa79a698a7ad2",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "9d0d221ac379c11a656a",
+    "url": "/markdown-previewer/static/css/main.d9731923.chunk.css"
+  },
+  {
+    "revision": "94bae008d0834ed003dc",
+    "url": "/markdown-previewer/static/js/2.553fd1ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.553fd1ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0d221ac379c11a656a",
+    "url": "/markdown-previewer/static/js/main.741df7b3.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
